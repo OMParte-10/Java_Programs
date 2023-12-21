22fa4b48bb588f2e79eea542f6e1cf766ce6b387
OMParte-10 <154506713+OMParte-10@users.noreply.github.com>
2023-12-21 14:29:31 +0530

    Add files via upload

---
+++ math.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class math{
+	public static void main(String args[])
+	{
+		System.out.print("Enter a number:");
+		Scanner reader = new Scanner(System.in);
+		int num = reader.nextInt();
+		String result = num > 0 ? "Positive number" : "Negative number";
+		System.out.println("Entered number is a "+ result);
+	}
+}
